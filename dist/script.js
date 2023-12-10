93d41a284bf8cc46ab5d93521669681bf4606673
rebahama <wiri@hotmail.se>
2023-12-10 22:18:40 +0000

    testing typescript files

--- dist/script.js
+++ dist/script.js
@@ -3,10 +3,30 @@ let age = 22;
 let namee = "jeff";
 let ageTwo = 50;
 let testingagain = "yes";
+const playerOne = {
+    name: "john",
+    score: 30,
+    age: 20
+};
+const playerTwo = {
+    name: "john",
+    score: 60,
+    age: 20
+};
+const newPlayers = [playerOne, playerTwo];
 console.log(age);
-function greeter(person) {
-    return "Hello, " + person;
-}
-let user = "Jane User";
-document.body.textContent = greeter(user);
+const names = [];
+names.push("Dylan");
+console.log(names);
+const hello = (newname) => {
+    return newname;
+};
+const multiply = (a, b) => {
+    return a * b;
+};
+console.log(multiply(3, 20));
+console.log(hello("name"));
+newPlayers.forEach((newPlayers) => {
+    console.log(newPlayers);
+});
 //# sourceMappingURL=script.js.map
